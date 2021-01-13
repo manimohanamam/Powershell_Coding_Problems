742b1e5264286d9b9537e4f51d53f4d06bcfbd1d
ManiMohan Amam <manimohan.amam@gmail.com>
2021-01-13 16:39:52 +0800

    Adding Scripts for Array Rotation

---
+++ Rotate-Array(Using Arraylists).ps1
@@ -0,0 +1,32 @@
+﻿<# 
+
+Objective: Write a Function to rotate the given Array with given no.of rotations
+Using ArrayLists
+
+Input = @(1,2,3,4,5) with no.of.rotations 
+Output = 5,1,2,3,4
+
+
+#>
+
+Function Rotate-Array()
+{
+    param([System.Collections.ArrayList]$input_array, [int]$no_of_rotations)
+
+    #$input_array
+
+    for($i = 0; $i -lt $no_of_rotations; $i++)
+    {
+        
+        $len_array = $input_array.Count
+
+        $array_element_to_be_rotated = $input_array[$len_array - 1]
+
+        $input_array.Remove($array_element_to_be_rotated) | Out-Null
+        $input_array.Insert(0,$array_element_to_be_rotated) | Out-Null
+    }
+    return $input_array
+}
+
+$array = @(1,2,3,4,5)
+Rotate-Array -input_array @(1,2,3,4,5) -no_of_rotations 5
---
+++ Rotate-Array(Using Arrays).ps1
@@ -0,0 +1,43 @@
+﻿<# Shift the Array on given input as no.of rotations
+Output = Array
+
+Input = @(1,2,3,4,5)
+
+5,1,2,3,4
+
+4,5,1,2,3
+
+1) record input - Array, rotations
+2) Iterate the array 
+
+#>
+
+Function Rotate-Array()
+{
+    param([array]$input_array, [int]$no_of_rotations)
+
+    for($i = 0; $i -lt $no_of_rotations; $i++)
+    {
+        
+        $len_array = $input_array.Length
+
+        $array_element_to_be_rotated = $input_array[$input_array.Length-1]
+
+        $Output_array = @()
+        $Output_array += $array_element_to_be_rotated
+
+        for($j = 0; $j -lt $input_array.Length-1; $j++)
+        {
+            if($input_array[$j] -ne $Output_array[$j])
+            {
+                $Output_array += $input_array[$j]
+            }
+        }
+
+        $input_array = $Output_array
+    }
+    Write-Host "$Output_array"
+    #return $Output_array
+}
+
+Rotate-Array -input_array @(1,2,3,4,5) -no_of_rotations 4
